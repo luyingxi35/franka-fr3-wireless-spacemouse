a6e1606807d7cb79f21552317a1822c11e31ef51
Andreas Gassner <andreas.gassner@franka.de>
2025-04-03 11:24:18 +0000

    XRL-391 feat: add multi-mouse support

--- src/spacemouse_publisher/launch/spacemouse_publisher.launch.py
+++ src/spacemouse_publisher/launch/spacemouse_publisher.launch.py
@@ -10,15 +10,23 @@ def generate_launch_description():
         description='Set to True if the operator is in the front position, otherwise False'
     )
 
+    device_path_arg = DeclareLaunchArgument(
+        'device_path',
+        default_value='',
+        description='Device path for the SpaceMouse'
+    )
+
     return LaunchDescription([
         operator_position_front_arg,
+        device_path_arg,
         Node(
             package='spacemouse_publisher',
             executable='pyspacemouse_publisher',
             name='spacemouse_publisher',
             output='screen',
             parameters=[
-                {'operator_position_front': LaunchConfiguration('operator_position_front')}
+                {'operator_position_front': LaunchConfiguration('operator_position_front')},
+                {'device_path': LaunchConfiguration('device_path')}
             ]
         )
     ])
